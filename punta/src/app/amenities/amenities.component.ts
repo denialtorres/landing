d9de77ed9f81fa8495943625621138046e18a1c4
Juan Carlos <jcarlos@masfusion.com>
2017-08-16 17:09:50 -0500

    Section Centenario

---
+++ punta/src/app/amenities/amenities.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amenities',
+  templateUrl: './amenities.component.html',
+  styleUrls: ['./amenities.component.sass']
+})
+export class AmenitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  images = ['1.jpg', '2.jpg', '3.jpg'];
+}
